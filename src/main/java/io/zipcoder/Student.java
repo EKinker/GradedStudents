02a0f61a1ccda0f08c7e0cd447c858d5c3fc5811
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-22 16:15:15 -0400

    Started Student Class

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,5 +1,6 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,7 +16,7 @@ public class Student {
     public Student(String firstName, String lastName, Double[] testScores){
         this.firstName = firstName;
         this.lastName = lastName;
-        this.examScores = Arrays.asList(testScores);
+        this.examScores= new ArrayList(Arrays.asList(testScores));
 
     }
     public String getFirstName() {
@@ -32,10 +33,28 @@ public class Student {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    public List<Double> getExamScores(){
-        return examScores;
+    public String getExamScores(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Exam Scores:\n");
+        for(Double d : examScores){
+           sb.append(String.format("%9s %d -> %.0f\n","Exam",(examScores.indexOf((d))+1),d));
+        }
+        return sb.toString();
     }
     public int getNumberOfExamsTaken(){
         return examScores.size();
+    }
+
+    public void addExamScore(double examScore){
+        examScores.add(examScore);
+    }
+
+    public void setExamScores(int examNum, double examScore){
+        examScores.set(examNum-1, examScore);
+    }
+
+    public double getAverageExamScore() {
+
+return 0;
     }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,24 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
 public class StudentTest {
+
+    @Test
+    public void testGetExamScores(){
+        String firstName = "Arthur";
+        String lastName = "Dent";
+        Double[] examScores = {75.0, 83.0, 89.0};
+        Student student = new Student(firstName, lastName, examScores);
+
+       // String output = student.getExamScores();
+        //System.out.println(output);
+        student.addExamScore(100.00);
+        System.out.println(student.getExamScores());
+        System.out.println(student.getNumberOfExamsTaken());
+
+        student.setExamScores(1,500);
+        System.out.println(student.getExamScores());
+    }
 
 }
