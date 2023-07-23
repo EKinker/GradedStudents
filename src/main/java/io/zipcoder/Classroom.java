152e70bbbdbdbbb0d95683883ddb304638f78288
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-22 20:35:17 -0400

    So many students

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,69 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Classroom {
+
+    private Student[] students;
+
+    public Classroom(int maxNumOfStudents) {
+        this.students = new Student[maxNumOfStudents];
+    }
+
+    public Classroom(Student[] students) {
+        this.students = students;
+    }
+
+    public Classroom() {
+        this.students = new Student[30];
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public double getAverageExamScore() {
+        double sum = 0;
+        for (Student s : students) {
+            sum += s.getAverageExamScore();
+        }
+        return sum / students.length;
+    }
+
+    public void addStudent(Student student) {
+        int nullInd = -1;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] == null) {
+                nullInd = i;
+                break;
+            }
+        }
+        if (nullInd >= 0) {
+            students[nullInd] = student;
+        } else {
+            Student[] newStudentArray = Arrays.copyOf(students, students.length + 1);
+            newStudentArray[students.length] = student;
+            students = newStudentArray;
+        }
+    }
+
+    public void removeStudent(String firstName, String lastName) {
+        int nullInd = -1;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].getFirstName().equalsIgnoreCase(firstName) && students[i].getLastName().equalsIgnoreCase(lastName)) {
+                nullInd = i;
+                students[i] = null;
+                break;
+            }
+        }
+        for (int i = nullInd + 1; i < students.length; i++) {
+            if (students[i - 1] == null) {
+                students[i - 1] = students[i];
+                students[i] = null;
+            }
+        }
+
+    }
+
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,29 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class ClassroomTest {
+
+@Test
+        public void test() {
+    // : Given
+    // : Given
+    int maxNumberOfStudents = 2;
+    Classroom classroom = new Classroom(maxNumberOfStudents);
+    Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+    Student student = new Student("Leon", "Hunter", examScores);
+    Student student1 = new Student("a", "b", examScores);
+    Student student2 = new Student("Lisa", "Simpson", examScores);
+    classroom.addStudent(student);
+    classroom.addStudent(student1);
+    classroom.addStudent(student2);
+
+    classroom.removeStudent("Leon", "Hunter");
+    classroom.removeStudent("Lisa", "Simpson");
+    System.out.println(classroom.getStudents().length);
+    System.out.println(Arrays.toString(classroom.getStudents()));
+
+}
 }
